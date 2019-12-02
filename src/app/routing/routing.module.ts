a0d1f2db3f5169412507fd4e7965b531e50f4b2d
TheProtagonist <38832704+GrimZero@users.noreply.github.com>
2019-12-02 08:50:22 +0100

    fixed 404?

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NotFoundComponent } from '../pages/not-found/not-found.component';
-import { ProjectsComponent } from '../pages/projects/projects.component';
 import { ProjectInfoComponent } from '../page-components/project-info/project-info.component';
+import { HomepageComponent } from '../pages/homepage/homepage.component';
 const routes = [
   { path: 'DeputyDonut', component: ProjectInfoComponent, data: {id: 'DeputyDonut'} },
-  { path: '', component: ProjectsComponent },
+  { path: '', component: HomepageComponent },
   { path: '**', component: NotFoundComponent }
 ];
 

38f405e6e65a24a87a137e1799bd66fa721ace54
TheProtagonist <38832704+GrimZero@users.noreply.github.com>
2020-01-16 17:11:59 +0100

    updatd projects info

--- src/app/interfaces/iproject.ts
+++ src/app/interfaces/iproject.ts
@@ -1,11 +1,12 @@
 export interface IProject {
     title?: string;
     preview?: string;
-    header?: string;
     route?: string;
     alternatePreview?: string;
     softwareUsed?: string[];
-    body?: string[];
     thumbnailSize?: number;
-    summary?: string;
+
+    purpose?: string;
+    objective?: string;
+    implementation?: string;
 }

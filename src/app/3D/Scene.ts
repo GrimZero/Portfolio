ce07a8b8a56f3b8c67b2819b3e41989d5d79b7a6
Torben Van Assche <torbenvanassche.anime@gmail.com>
2020-03-20 23:53:48 +0100

    fixed whiteboard lighting. changed icon on projects.

--- src/app/3D/Scene.ts
+++ src/app/3D/Scene.ts
@@ -11,7 +11,7 @@ export abstract class Scene extends THREE.Scene {
 
     addDirectionalLight(position: THREE.Vector3, color: string | number | THREE.Color = 0xFFFFFF, intensity: number = 1,
                         castShadow: boolean = false, nearClip: number = 0.1, farClip: number = 1000,
-                        size: THREE.Vector2 = new THREE.Vector2(100, 100), bias: number = 0) {
+                        size: THREE.Vector2 = new THREE.Vector2(100, 100), bias: number = -0.0001) {
         const light = new THREE.DirectionalLight(color, intensity);
 
         if (castShadow) {

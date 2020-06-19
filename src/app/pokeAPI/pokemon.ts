2019d0ab4028a410676c40410e2ba1ddc0a5918c
Torben Van Assche <38832704+GrimZero@users.noreply.github.com>
2020-06-19 09:46:27 +0200

    separated into different repo

--- src/app/pokeAPI/pokemon.ts
+++
@@ -1,8 +0,0 @@
-import * as PokemonAPI from 'pokeapi-js-wrapper';
-
-export class Pokemon {
-    constructor() {
-        const P = new PokemonAPI.Pokedex();
-        console.log(P.getPokemonsList());
-    }
-}
